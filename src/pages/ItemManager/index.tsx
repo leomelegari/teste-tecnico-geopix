d9269878d5eac0994696cecaaccebed76cf22425
Leonardo Melegari Deffende <leo_melegari@outlook.com>
2022-08-02 15:56:52 -0300

    feat: Item manager finished, just need to apply the styles

--- src/pages/ItemManager/index.tsx
+++ src/pages/ItemManager/index.tsx
@@ -1,3 +1,89 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { v4 as uuidv4 } from "uuid";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+interface ItemProp {
+  id: string;
+  name: string;
+}
+
+const schema = yup.object().shape({
+  name: yup
+    .string()
+    .required("Campo obrigatório")
+    .min(3, "Digite ao menos 3 caracteres"),
+});
+
 export const ItemManager = () => {
-  return <div>ItemManager</div>;
+  const [items, setItems] = useState<ItemProp[]>([]);
+  const [selected, setSelected] = useState("");
+
+  const {
+    handleSubmit,
+    reset,
+    register,
+    formState: { errors },
+  } = useForm<ItemProp>({ resolver: yupResolver(schema) });
+
+  const handleAddItem = (data: ItemProp) => {
+    if (data.name.length > 0) {
+      const newItem: ItemProp = {
+        id: uuidv4(),
+        name: data.name,
+      };
+
+      setItems((state) => [...state, newItem]);
+      reset();
+    }
+  };
+
+  const handleRemoveItem = (data: string) => {
+    const newItemList = items.filter((item) => item.id !== data);
+    setItems(newItemList);
+  };
+
+  const handleClearBox = () => {
+    setItems([]);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(handleAddItem)}>
+        <div>
+          <select
+            placeholder="Selecione uma opção"
+            onChange={(e) => setSelected(e.target.value)}
+          >
+            <option value="default">Selecione uma opção</option>
+            {items.map((item) => {
+              return (
+                <option key={item.id} value={item.id}>
+                  {item.name}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Insira um item"
+            {...register("name")}
+          />
+          <span>{errors.name?.message}</span>
+        </div>
+        <div>
+          <button type="submit">Adicionar</button>
+          <button type="button" onClick={() => handleRemoveItem(selected)}>
+            Remover
+          </button>
+          <button type="button" onClick={handleClearBox}>
+            Limpar caixa
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
